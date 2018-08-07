a2618816076b0608968633b805db3e99be782179
Your NameWillanzhang <631871612@qq.com>
2018-08-07 17:59:59 +0800

    'webapck配置优化'

--- build/webpack.config.client.js
+++ build/webpack.config.client.js
@@ -1,36 +1,16 @@
 const path = require('path') // 使用path
 const HTMLPlugin = require('html-webpack-plugin') // 生成html 并且将output 引入
 const webpack = require('webpack')
+const baseConfig = require('./webpack.base')
+const webpackMerge = require('webpack-merge')
 const isDev = process.env.NODE_ENV === 'development'
-const config = {
+console.log('---', webpackMerge)
+const config = webpackMerge(baseConfig, {
   entry: {
     app: path.join(__dirname, '../client/app.js') // 入口 文件 利用path使用绝对路径 避免发生错误（环境）
   },
   output: {
-    filename: '[name].[hash].js',
-    path: path.join(__dirname, '../dist'),
-    publicPath: '/public/' // 静态资源引用的路径 区分静态资源请求和api 以及配置nginx 图片部署到cdn上处理等 ---/public/app.hash.js   必须是'/public/'（注意后面的斜杠）否则开发环境热更新会有问题
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre', // pre babel-loader之前先执行 eslint-loader
-        test: /.(js|jsx)$/,
-        loader: 'eslint-loader',
-        exclude: [
-          path.join(__dirname, '../node_modules')
-        ] // 不编译 node_modules 文件下的js 和 jsx 文件，
-      },
-      {
-        test: /.(jsx|js)$/,   // 需要使用loader 的文件
-        loader: 'babel-loader',
-        exclude: path.join(__dirname, '../node_modules') // 不编译 node_modules 文件下的js 和 jsx 文件，
-        // include: path.join(__dirname, '../client') // 只编译 clinet 文件下的js 和 jsx 文件，
-      }
-    ]
+    filename: '[name].[hash].js'
   },
   performance: {
     hints: false
@@ -44,8 +24,8 @@ const config = {
       filename: 'server.ejs'
     })
   ]
-}
-
+})
+console.log('///***',config)
 if (isDev) {
   config.entry = {  // dev环境热加载需要改变entry
     app: [
--- build/webpack.config.server.js
+++ build/webpack.config.server.js
@@ -1,35 +1,14 @@
 const path = require('path') // 使用path
+const baseConfig = require('./webpack.base')
+const webpackMerge = require('webpack-merge')
 
-module.exports = {
+module.exports = webpackMerge(baseConfig, {
   target: 'node',  // 打包后的文件是使用在什么地方
   entry: {
     app: path.join(__dirname, '../client/server-entry.js') // 入口 文件 利用path使用绝对路径 避免发生错误（环境）
   },
   output: {
     filename: 'server-entry.js',
-    path: path.join(__dirname, '../dist'),
-    publicPath: '/public', // 静态资源引用的路径 区分静态资源请求和api 以及配置nginx 图片部署到cdn上处理等 ---/public/app.hash.js
-    libraryTarget: 'commonjs2'  // 打包后的js的模块方案  'amd'-requirejs  'cmd'-seajs 'umd'  'commonjs'    
-  },
-  resolve: {
-    extensions: ['.js', '.jsx'],
-  },
-  module: {
-    rules: [
-      {
-        enforce: 'pre', // pre babel-loader之前先执行 eslint-loader
-        test: /.(js|jsx)$/,
-        loader: 'eslint-loader',
-        exclude: [
-          path.join(__dirname, '../node_modules')
-        ] // 不编译 node_modules 文件下的js 和 jsx 文件，
-      },
-      {
-        test: /.(jsx|js)$/,   // 需要使用loader 的文件
-        loader: 'babel-loader',
-        exclude: path.join(__dirname, '../node_modules') // 不编译 node_modules 文件下的js 和 jsx 文件，
-        // include: path.join(__dirname, '../client') // 只编译 clinet 文件下的js 和 jsx 文件，
-      }
-    ]
+    libraryTarget: 'commonjs2'  // 打包后的js的模块方案  'amd'-requirejs  'cmd'-seajs 'umd'  'commonjs'
   }
-}
+})
